c07e26317ae166b011caafd9e5633ab5a361d229
Bernardo Lucas Almeida Sunderhus <bernardo.sunderhus@gmail.com>
2019-06-27 21:40:45 +0000

    Remove security issues on dependencies

--- src/web-components/index.tsx
+++
@@ -1,4 +0,0 @@
-export {default as HighlightText} from './highlight-text'
-export {default as PortDetails} from './port-details'
-export {default as PortEvent} from './port-event'
-export {PortList, PortListItem} from './port-list'
